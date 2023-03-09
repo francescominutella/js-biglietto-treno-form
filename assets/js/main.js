c1aabb9e2b7fcd1a00a44c67b0b6042031055704
Fra <eso.fra24@gmail.com>
2023-03-09 16:21:49 +0100

    output needs some way to be view in page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,29 @@
 
+function btnGetPrice() {
+    //var declaretion
+    let kilometers
+    let age
+    const priceKm = 0.21;
+    let ticketPrice = kilometers * priceKm;
+    let totalPrice
 
+    if (age < 18) {
+        totalPrice = 0.2;
 
-let kilometers
+    } else if (age > 65) {
+        totalPrice = 0.4;
 
-let age
+    } else if (age >= 18 && age <= 65) {
+        totalPrice = 0;
+    }
 
-const priceKm = 0.21;
+    //var declaretion
+    const discountPrice = ticketPrice * totalPrice
+    const resultEl = document.getElementById("result");
 
-let ticketPrice = kilometers * priceKm;
-
-let totalPrice
-
-
-if (age < 18) {
-    totalPrice = 0.2;
-
-} else if (age > 65) {
-    totalPrice = 0.4;
-
-} else if (age >= 18 && age <= 65) {
-    totalPrice = 0;
-}
-
-const discountPrice = ticketPrice * totalPrice
-
-
-
-const resultEl = document.getElementById("result");
-
-if (isNaN(kilometers)){
-    resultEl.innerHTML = "Error!!! Enter only number."
-} else{
-    resultEl.innerHTML = `Price: ${discountPrice.toFixed(2)} € `;
+    if (isNaN(kilometers)) {
+        resultEl.innerHTML = "Error!!! Enter only number."
+    } else {
+        resultEl.innerHTML = `Price: ${discountPrice.toFixed(2)} € `;
+    }
 }
